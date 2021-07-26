2b4768fec4f3d40014e5d64e5f0e62c14429cdee
Ravi Kumar Singh <rksingh0100@outlook.com>
2021-07-26 15:23:08 +0530

    changed for test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Kubernetes Cluster Fib Calculator
+            Kubernetes Cluster Fib Calculator vipul
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
