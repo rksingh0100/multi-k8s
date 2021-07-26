fc1cf696f85eaf6886eb63930dea0392169750eb
Ravi Kumar Singh <rksingh0100@outlook.com>
2021-07-26 10:06:13 +0530

    changed header

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            Kubernetes Cluster Fib Calculator
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
